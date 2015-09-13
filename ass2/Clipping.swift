83f8e3b552badfe7e88d06c07f11974bbf3ada93
WEI <sam-123-123@163.com>
2015-09-13 22:48:00 +1000

    p1

---
+++ ass2/Clipping.swift
@@ -0,0 +1,16 @@
+//
+//  Clipping.swift
+//  ass2
+//
+//  Created by WEI on 15/9/13.
+//  Copyright (c) 2015年 WEI. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Clipping: NSManagedObject {
+
+    @NSManaged var owner: NSManagedObject
+
+}
---
+++ ass2/Collection.swift
@@ -0,0 +1,17 @@
+//
+//  Collection.swift
+//  ass2
+//
+//  Created by WEI on 15/9/13.
+//  Copyright (c) 2015年 WEI. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Collection: NSManagedObject {
+
+    
+    @NSManaged var clipping: NSSet
+
+}
