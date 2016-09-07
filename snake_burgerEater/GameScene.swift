7ce9c9cf5145d1a068dfd46fd850194a1d9a612b
Your Name <WEI>
2016-09-07 12:43:38 +1000

    hi

--- snake_burgerEater/GameScene.swift
+++ snake_burgerEater/GameScene.swift
@@ -1,45 +1,177 @@
 //
 //  GameScene.swift
-//  snake_burgerEater
+//  Demo
 //
-//  Created by WEI on 9/7/16.
+//  Created by Chenhao Wei on 20/08/16.
 //  Copyright (c) 2016 WEI. All rights reserved.
 //
 
 import SpriteKit
 
-class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
-        
-        self.addChild(myLabel)
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+class GameScene: SKScene, SKPhysicsContactDelegate {
+	
+	// MARK: - Instance Variables
+	
+	let playerSpeed: CGFloat = 150.0
+	let zombieSpeed: CGFloat = 75.0
+	
+	var goal: SKSpriteNode?
+	var player: SKSpriteNode?
+	var zombies: [SKSpriteNode] = []
+	
+	var lastTouch: CGPoint? = nil
+	
+	
+	// MARK: - SKScene
+	
+	override func didMoveToView(view: SKView) {
+		// Setup player
+		player = self.childNodeWithName("player") as? SKSpriteNode
+		
+		
+		
+		// Setup zombies
+		for child in self.children {
+			if child.name == "zombie" {
+				if let child = child as? SKSpriteNode {
+					zombies.append(child)
+				}
+			}
+		}
+		
+		
+		// Setup physics world's contact delegate
+		physicsWorld.contactDelegate = self
+		
+		// Setup initial camera position
+		updateCamera()
+	}
+	
+	
+	// MARK: Touch Handling
+	
+	override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		handleTouches(touches)
+	}
+	
+	override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		handleTouches(touches)
+	}
+	
+	override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+		handleTouches(touches)
+	}
+	
+	private func handleTouches(touches: Set<UITouch>) {
+		for touch in touches {
+			let touchLocation = touch.locationInNode(self)
+			lastTouch = touchLocation
+		}
+	}
+	
+	
+	// MARK - Updates
+	
+	override func didSimulatePhysics() {
+		if let _ = player {
+			updatePlayer()
+			updateZombies()
+		}
+	}
+	
+	// Determines if the player's position should be updated
+	private func shouldMove(currentPosition currentPosition: CGPoint, touchPosition: CGPoint) -> Bool {
+		return abs(currentPosition.x - touchPosition.x) > player!.frame.width / 2 ||
+			abs(currentPosition.y - touchPosition.y) > player!.frame.height/2
+	}
+	
+	// Updates the player's position by moving towards the last touch made
+	func updatePlayer() {
+		if let touch = lastTouch {
+			let currentPosition = player!.position
+			if shouldMove(currentPosition: currentPosition, touchPosition: touch) {
+				
+				let angle = atan2(currentPosition.y - touch.y, currentPosition.x - touch.x) + CGFloat(M_PI)
+				let rotateAction = SKAction.rotateToAngle(angle + CGFloat(M_PI*0.5), duration: 0)
+				
+				player!.runAction(rotateAction)
+				
+				let velocotyX = playerSpeed * cos(angle)
+				let velocityY = playerSpeed * sin(angle)
+				
+				let newVelocity = CGVector(dx: velocotyX, dy: velocityY)
+				player!.physicsBody!.velocity = newVelocity;
+				updateCamera()
+			} else {
+				player!.physicsBody!.resting = true
+			}
+		}
+	}
+	
+	func updateCamera() {
+		if let camera = camera {
+			camera.position = CGPoint(x: player!.position.x, y: player!.position.y)
+		}
+	}
+	
+	// Updates the position of all zombies by moving towards the player
+	func updateZombies() {
+		let targetPosition = player!.position
+		
+		for zombie in zombies {
+			let currentPosition = zombie.position
+			
+			let angle = atan2(currentPosition.y - targetPosition.y, currentPosition.x - targetPosition.x) + CGFloat(M_PI)
+			let rotateAction = SKAction.rotateToAngle(angle + CGFloat(M_PI*0.5), duration: 0.0)
+			zombie.runAction(rotateAction)
+			
+			let velocotyX = zombieSpeed * cos(angle)
+			let velocityY = zombieSpeed * sin(angle)
+			
+			let newVelocity = CGVector(dx: velocotyX, dy: velocityY)
+			zombie.physicsBody!.velocity = newVelocity;
+		}
+	}
+	
+	
+	// MARK: - SKPhysicsContactDelegate
+	
+	func didBeginContact(contact: SKPhysicsContact) {
+		// 1. Create local variables for two physics bodies
+		var firstBody: SKPhysicsBody
+		var secondBody: SKPhysicsBody
+		
+		// 2. Assign the two physics bodies so that the one with the lower category is always stored in firstBody
+		if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+			firstBody = contact.bodyA
+			secondBody = contact.bodyB
+		} else {
+			firstBody = contact.bodyB
+			secondBody = contact.bodyA
+		}
+		
+		// 3. react to the contact between the two nodes
+		if firstBody.categoryBitMask == player?.physicsBody?.categoryBitMask &&
+			secondBody.categoryBitMask == zombies[0].physicsBody?.categoryBitMask {
+			// Player & Zombie
+			gameOver(false)
+		} else if firstBody.categoryBitMask == player?.physicsBody?.categoryBitMask &&
+			secondBody.categoryBitMask == goal?.physicsBody?.categoryBitMask {
+			// Player & Goal
+			gameOver(true)
+		}
+	}
+	
+	
+	// MARK: Helper Functions
+	
+	private func gameOver(didWin: Bool) {
+		print("- - - Game Ended - - -")
+		let menuScene = MenuScene(size: self.size)
+		menuScene.soundToPlay = didWin ? "fear_win.mp3" : "fear_lose.mp3"
+		let transition = SKTransition.flipVerticalWithDuration(1.0)
+		menuScene.scaleMode = SKSceneScaleMode.AspectFill
+		self.scene!.view?.presentScene(menuScene, transition: transition)
+	}
+	
 }
--- snake_burgerEater/GameViewController.swift
+++ snake_burgerEater/GameViewController.swift
@@ -1,53 +1,72 @@
 //
 //  GameViewController.swift
-//  snake_burgerEater
+//  Demo
 //
-//  Created by WEI on 9/7/16.
+//  Created by Chenhao Wei on 20/08/16.
 //  Copyright (c) 2016 WEI. All rights reserved.
 //
 
 import UIKit
 import SpriteKit
 
+extension SKNode {
+	
+	class func unarchiveFromFile(file : String) -> SKNode? {
+		if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
+			let sceneData = try! NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe)
+			let archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
+			
+			archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+			let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
+			archiver.finishDecoding()
+			return scene
+		} else {
+			return nil
+		}
+	}
+	
+}
+
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
-    }
-
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    override func prefersStatusBarHidden() -> Bool {
-        return true
-    }
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
+			// Configure the view.
+			let skView = self.view as! SKView
+			skView.showsFPS = true
+			skView.showsNodeCount = true
+			
+			/* Sprite Kit applies additional optimizations to improve rendering performance */
+			skView.ignoresSiblingOrder = true
+			
+			/* Set the scale mode to scale to fit the window */
+			scene.scaleMode = .AspectFill
+			
+			skView.presentScene(scene)
+		}
+	}
+	
+	override func shouldAutorotate() -> Bool {
+		return true
+	}
+	
+	override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+		if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+			return UIInterfaceOrientationMask.AllButUpsideDown
+		} else {
+			return UIInterfaceOrientationMask.All
+		}
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Release any cached data, images, etc that aren't in use.
+	}
+	
+	override func prefersStatusBarHidden() -> Bool {
+		return true
+	}
+	
 }
